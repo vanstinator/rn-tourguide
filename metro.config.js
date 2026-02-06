62532c85832a8bed1b988b1ed48e784c2a1d310a
vanstinator <justinvdhooft@gmail.com>
2026-02-06 13:34:50 -0500

    dependencies: upgrade to expo 54

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  resolver: {
-    sourceExts: ['js', 'json', 'jsx', 'ts', 'tsx'],
-  },
-}
+const { getDefaultConfig } = require('expo/metro-config')
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname)
+
+module.exports = config
